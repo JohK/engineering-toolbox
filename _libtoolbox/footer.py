063d729977150b0b2dfbc735fc2345af831ca5e8
Johannes Kneer <johannes.kneer@nuflo.de>
2021-01-29 14:45:35 +0100

    Larger plots, added footer

---
+++ _libtoolbox/footer.py
@@ -0,0 +1,8 @@
+from IPython.display import display, Markdown
+
+#</div>
+footer = '''
+<div style='position: fixed; left: 0; bottom: 0; width: 100%; background-color: #f7b500; color: #000000; text-align: center; vertical-align: middle; padding: 10px'> 
+<b><a style='color: #000000;' href="https://nuflo.de/"><img src="../_libtoolbox/nuflo-logo-N-512x512.png" alt="logo-N" width="16px" />&nbsp;&nbsp;&nbsp;nuflo</a></b></div>'''
+
+display(Markdown(footer))
--- _libtoolbox/header.py
+++ _libtoolbox/header.py
@@ -2,13 +2,14 @@
 
 from IPython.display import display, Markdown
 
-header = '''&nbsp;[<img hspace="45%" align="middle" src="../_libtoolbox/nuflo-logo-N-512x512.png" alt="logo-N" width="8%" />](https://nuflo.de)
+header = '''&nbsp;<a href="https://nuflo.de"><img hspace="45%" align="middle" src="../_libtoolbox/nuflo-logo-N-512x512.png" alt="logo-N" width="8%" /></a>
 
 <center><h1>nuflo Engineering Toolbox</h1></center>
 
 [nuflo is an engineering company](https://nuflo.de) that supports digitalization of engineering processes in fluid mechanics.
 
 *The code for this tool can be found over at [github](https://github.com/JohK/engineering-toolbox).*  
-*More interactive tools are located at [toolbox.nuflo.de](http://toolbox.nuflo.de).*'''
-
+*More interactive tools are located at [toolbox.nuflo.de](http://toolbox.nuflo.de).*
+'''
+#div style='margin-top: auto;'>
 display(Markdown(header))
