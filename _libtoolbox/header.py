0862bdccb6a8902c6da194a53e216bd4a2a03fda
Johannes Kneer <johannes.kneer@nuflo.de>
2021-02-02 21:40:42 +0100

    Added __version__

--- _libtoolbox/header.py
+++ _libtoolbox/header.py
@@ -8,8 +8,12 @@ header = '''&nbsp;<a href="https://nuflo.de"><img hspace="45%" align="middle" sr
 
 [nuflo is an engineering company](https://nuflo.de) that supports digitalization of engineering processes in fluid mechanics.
 
+After a period of inactivity you may need to reload the webpage for the tool to work properly. If you find yourself using these tools a lot, consider a local installation. Please feel free to contact us at johannes.kneer@nuflo.de for support.
+
 *The code for this tool can be found over at [github](https://github.com/JohK/engineering-toolbox).*  
 *More interactive tools are located at [toolbox.nuflo.de](http://toolbox.nuflo.de).*
+
+
 '''
 #div style='margin-top: auto;'>
 display(Markdown(header))
